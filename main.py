d8ec861badc44ed5726d3ec639340fc5f23da242
Dastan <3052428@gmail.com>
2024-12-04 15:08:55 +0500

    Поменял порт на 8888

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ async def get_all_chart(
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8888, reload=True)
